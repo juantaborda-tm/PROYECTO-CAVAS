001611d6b8f68f739007cc6717989c0fc7192aaa
alanpo1236 <alansmithhernandezpaez@gmail.com.com>
2025-10-23 19:58:03 -0500

    nuevo backend

--- conexion.py
+++ conexion.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request
-import mysql.connector
+import mysql.connector, django
 
 my_db = mysql.connector.connect(host = "localhost",
                                 port = "3306",
@@ -24,15 +24,23 @@ def registro_g():
     id_tipo_gerente = request.form["id_tipo_gerente"]
     tel_gerente = request.form["tel_gerente"]
     contraseña_gerente = request.form["contraseña_gerente"]
-    nit_empresa = request.form["nit_empresa"]
+    nit_emprese = request.form["nit_empresa"]
     fecha_nacimiento_g = request.form["fecha_nacimiento_g"]
     id_gerente = request.form["id_gerente"]
     email_gerente = request.form["email_gerente"]
+    roll = "1" #roll 1 es gerente
     cursor = my_db.cursor()
-    sql = f"INSERT INTO usuarios (nombre_empresa, nombre, identificacion, fecha_nacimiento_g, telefono, contraseña, nit_empresa, id_gerente, email_gerente) VALUE('{nombre_empresa}', '{nit_empresa}','{nombre_gerente}', '{fecha_nacimiento_g}','{id_tipo_gerente}', {id_gerente}', '{tel_gerente}','{email_gerente}' '{contraseña_gerente}', )"
+    #corregir orden de sintaxys SQL
+    sql = f"INSERT INTO usuarios (num_id, nom_comple, correo, contra, nom_empresa, num_tel, nit_empre, fecha_naci, tipo_id, roll) VALUES ('{id_gerente}' , '{nombre_gerente}' , '{email_gerente}' , '{contraseña_gerente}' , '{nombre_empresa}' , '{tel_gerente}' , '{nit_emprese}' , '{fecha_nacimiento_g}' , '{id_tipo_gerente}' , '{roll}')"
     cursor.execute(sql)
     my_db.commit()
-    return redirect("/")
+    return render_template("registro_gerente.html")
+
+
+@programa.route("/registro_u")
+
+
+
 
 
 
